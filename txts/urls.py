9bbc39054450a84e3cc98c8f5cd3c3f3c7cb9ea8
Jordi Funollet <jordi.f@ati.es>
2007-01-27 12:14:18 +0000

    URLs with - were'nt matched.

--- txts/urls.py
+++ txts/urls.py
@@ -15,14 +15,14 @@ date_dict = dict( archive_dict, month_format='%m', slug_field='permalink' )
 urlpatterns = patterns('txts.views',
     (r'^txt-preview/$', 'preview',),
     #
-    (r'^(?P<section>\w+)/$',
+    (r'^(?P<section>[\-\w]+)/$',
         'list_detail_object_list', info_dict,
     ),
-    (r'^(?P<section>\w+)/(?P<slug>[\-\w]+)/$',
+    (r'^(?P<section>[\-\w]+)/(?P<slug>[\-\w]+)/$',
         'list_detail_object_detail', dict(info_dict, slug_field="permalink"),
     ),
     # <section>/<year>/<month>/<day>/<permalink>/
-    (r'^(?P<section>\w+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>\w+)/$',
+    (r'^(?P<section>[\-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\-\w]+)/$',
         'date_based_object_detail', date_dict,
     ),
 )
