e7282ba146b84302a106661328ce1d6e3563203d
Jordi Funollet <jordi.f@ati.es>
2007-06-19 17:21:48 +0000

    txts: added url for tags.

--- txts/urls.py
+++ txts/urls.py
@@ -1,23 +1,34 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls.defaults import *
 from txts.models import Txt, TxtSection
+from tags.models import Tag
 
 info_dict = { 
-    'queryset': Txt.public.all(), 
+    'queryset': Txt.public.all().order_by('-pub_date'), 
 }
 
 archive_dict = {
     'date_field': 'pub_date',
-    'queryset': Txt.public.all(),
+    'queryset': Txt.public.all().order_by('-pub_date'),
 }
 date_dict = dict( archive_dict, month_format='%m', slug_field='permalink' )
 
-urlpatterns = patterns('txts.views',
+info_tags = { 
+    'queryset': Tag.objects.all(),
+}
+
+
+urlpatterns = []
+urlpatterns += patterns('txts.views',
+    (r'^txt-preview/$', 'preview',),
+    #
+    (r'^tags/(?P<tag>.*)/$', 'tag_list', info_dict),
+    #
     (r'^(?P<section>[\-\w]+)/$',
         'list_detail_object_list', info_dict,
     ),
     (r'^(?P<section>[\-\w]+)/(?P<slug>[\-\w]+)/$',
-        'list_detail_object_detail', dict(info_dict, slug_field="permalink"),
+        'list_detail_object_detail', dict(info_dict, slug_field='permalink'),
     ),
     # <section>/<year>/<month>/<day>/<permalink>/
     (r'^(?P<section>[\-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\-\w]+)/$',
@@ -26,5 +37,10 @@ urlpatterns = patterns('txts.views',
 )
 
 urlpatterns += patterns('',
-    (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/blog/'}),
+    (r'^$', 'django.views.generic.list_detail.object_list', {
+        'queryset': Txt.public.all().filter(section__permalink='blog'
+            ).order_by('-pub_date')[:1],
+        'template_name': 'txts/home_list.html',
+    } ),
+    #(r'^$', 'django.views.generic.simple.redirect_to', {'url': '/blog/'}),
 )
