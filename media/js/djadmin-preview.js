337cfce5961bca25af610d5f712263cc1ba1e04c
Jordi Funollet <jordi.f@ati.es>
2007-09-06 00:07:45 +0000

    Upgraded to jquery-1.1.4, farbtastic-1.2, jquery.form-trunk

--- media/js/djadmin-preview.js
+++ media/js/djadmin-preview.js
@@ -1,19 +1,18 @@
-// Don't forget loading jquery.js and form.js (jQuery Form plugin).
+// Don't forget loading jquery.js and jquery.form.js (jQuery Form plugin).
 function show_preview () {
-    var form_fields = $("form #txt_form").formToArray();
+    var form_fields = $("form#txt_form").formToArray();
     $.post("/preview/", form_fields, function(xml) {
         $("#preview-content").html(xml) ;
-        $("#preview").show() ;
     });
-    
+    $("#preview").show("slow") ;
     return(false) ;
 }
 
 function hide_preview () {
-    $("#preview").hide();
-    return (false);
+    $("#preview").hide("slow");
+    return(false);
 }
 
-$(document).ready( function () {
-    $("a #button-preview").toggle(show_preview, hide_preview) ;
+$(document).ready( function (){
+    $("a#button-preview").toggle(show_preview, hide_preview) ;
 });
