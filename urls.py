23ea3467e91d0a1f1b1e80d936e0b422b785a546
Jordi Funollet <jordi.f@ati.es>
2007-12-02 20:46:22 +0000

    Per year, month and day archives.

--- urls.py
+++ urls.py
@@ -4,25 +4,40 @@ from django.conf.urls.defaults import *
 from txts.models import Txt, TxtSection
 
 
+date_based_params = {
+    'queryset': Txt.public.all(), 
+    'date_field': 'pub_date',
+}
+
+
 urlpatterns = []
 urlpatterns += patterns('',
     (r'^tag/(?P<tag>[^/]+(?u))/$', 'tagging.views.tagged_object_list', 
         dict( model=Txt, allow_empty=True, template_name='txts/tag_list.html', ),
     )
 )
+
+
 urlpatterns += patterns('txts.views',
     (r'^preview/$', 'preview',),
     #
+    # Date-based archives.
+    (r'^(?P<section>[\-\w]+)/(?P<year>\d{4})/$',
+        'date_based.archive_year', date_based_params,
+    ),
+    (r'^(?P<section>[\-\w]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/$',
+        'date_based.archive_month', dict( date_based_params, month_format='%m', ), 
+    ),
+    (r'^(?P<section>[\-\w]+)/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
+        'date_based.archive_day', dict( date_based_params, month_format='%m', ), 
+    ),
+    #
+    # List-based views.
     (r'^(?P<section>[\-\w]+)/$', 'list_detail.object_list',
         dict( queryset=Txt.public.all(), ),
     ),
     (r'^(?P<section>[\-\w]+)/(?P<slug>[\-\w]+)/$', 'list_detail.object_detail',
          dict( queryset=Txt.public.all(), slug_field='easyname', ),
     ),
-    # <section>/<year>/<month>/<day>/<easyname>/
-    (r'^(?P<section>[\-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\-\w]+)/$',
-        'date_based.object_detail',
-        dict( queryset=Txt.public.all(), date_field='pub_date', month_format='%m', 
-            slug_field='easyname', ),
-    ),
+
 )
